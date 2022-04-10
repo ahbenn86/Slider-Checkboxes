f712a121642118660489048fdd602d15f911d73f
Angelo Bennett <77983302+ahbenn86@users.noreply.github.com>
2022-04-10 18:12:23 -0400

    html | css | script changes

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 const toggles = document.querySelectorAll('.toggle')
-const good = document.querySelector('#good')
-const cheap = document.querySelector('#cheap')
-const fast = document.querySelector('#fast')
+const ketchup = document.querySelector('#ketchup')
+const mustard = document.querySelector('#mustard')
+const mayo = document.querySelector('#mayo')
 
 toggles.forEach(toggle => toggle.addEventListener('change', (e) => doTheTrick(e.target)))
 
 function doTheTrick(theClickedOne){
-    if(good.checked && cheap.checked && fast.checked){
-        if(good === theClickedOne){
-            fast.checked = false 
+    if(ketchup.checked && mustard.checked && mayo.checked){
+        if(ketchup === theClickedOne){
+            mayo.checked = false 
         }
 
-        if(cheap === theClickedOne){
-            good.checked = false
+        if(mustard === theClickedOne){
+            ketchup.checked = false
         }
 
-        if(fast === theClickedOne){
-            cheap.checked = false
+        if(mayo === theClickedOne){
+            mustard.checked = false
         }
     }
 }
